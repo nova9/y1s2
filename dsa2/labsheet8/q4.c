5c1fae62863c997829f3ff923901e1c0a758de3c
nova9 <thathsaramadhhusha@gmail.com>
2024-01-19 11:41:41 +0530

    q4

--- dsa2/labsheet8/q4.c
+++ dsa2/labsheet8/q4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct Node Node;
 struct Node
@@ -30,8 +31,31 @@ int height(Node *root)
   return (l > r ? l : r) + 1;
 }
 
-int balanceFactor(Node *root){
+int balanceFactor(Node *root)
+{
   return height(root->left) - height(root->right);
+}
+
+Node *rightRotate(Node *y)
+{
+  Node *x = y->left;
+  Node *T2 = x->right;
+
+  x->right = y;
+  y->left = T2;
+
+  return x;
+}
+
+Node *leftRotate(Node *x)
+{
+  Node *y = x->right;
+  Node *T2 = y->left;
+
+  y->left = x;
+  x->right = T2;
+
+  return y;
 }
 
 Node *insert(Node *root, int key)
@@ -53,11 +77,57 @@ Node *insert(Node *root, int key)
 
   int bf = balanceFactor(root);
 
-  if (abs(bf) > 1) {
-    printf("%d not balanced %d\n", root->key, bf);
+  if (bf > 1 && key < root->left->key)
+    return rightRotate(root);
+
+  if (bf < -1 && key > root->right->key)
+    return leftRotate(root);
+
+  if (bf > 1 && key > root->left->key)
+  {
+    root->left = leftRotate(root->left);
+    return rightRotate(root);
+  }
+
+  if (bf < -1 && key < root->right->key)
+  {
+    root->right = rightRotate(root->right);
+    return leftRotate(root);
   }
 
   return root;
+}
+
+bool isBalanced(Node *root)
+{
+  if (root == NULL)
+  {
+    return 1;
+  }
+
+  int lh = height(root->left);
+  int rh = height(root->right);
+
+  if (abs(lh - rh) > 1)
+  {
+    return 0;
+  }
+
+  bool lb = isBalanced(root->left);
+  bool rb = isBalanced(root->right);
+
+  return lb && rb;
+}
+
+void preorder(Node *root)
+{
+  if (root == NULL)
+  {
+    return;
+  }
+  printf("%d ", root->key);
+  preorder(root->left);
+  preorder(root->right);
 }
 
 void inorder(Node *root)
@@ -72,13 +142,41 @@ void inorder(Node *root)
   inorder(root->right);
 }
 
+void postorder(Node *root)
+{
+  if (root == NULL)
+  {
+    return;
+  }
+
+  postorder(root->left);
+  postorder(root->right);
+  printf("%d ", root->key);
+}
+
 int main()
 {
   Node *root = NULL;
   root = insert(root, 2);
   root = insert(root, 3);
   root = insert(root, 4);
-  root = insert(root, 1);
+  root = insert(root, 13);
+  root = insert(root, 14);
+  root = insert(root, 0);
+  root = insert(root, 56);
+  root = insert(root, 8);
 
+  printf("Is Balanced %d\n", isBalanced(root));
+
+  printf("Preorder: ");
+  preorder(root);
+  printf("\n");
+
+  printf("Inorder: ");
   inorder(root);
+  printf("\n");
+
+  printf("Postorder: ");
+  postorder(root);
+  printf("\n");
 }
